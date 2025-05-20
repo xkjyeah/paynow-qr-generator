eecbf30b4397b86265bb97ecde56d0b1df39a324
dssq <danielssq89@gmail.com>
2025-05-20 12:31:54 +0800

    image quality

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,8 @@
 
   <main>
     <div class="no-print">
-      This page generates a QR for your phone number, or for your UEN.
-
-      Print it out and paste it wherever you run a hawker stall, pasar malam stall,
-      or a garage sale!
+      This page generates a QR for your phone number, or for your UEN. Print it out and
+      paste it wherever you run a hawker stall, pasar malam stall, or a garage sale!
     </div>
 
     <div class="field no-print">
@@ -26,30 +24,45 @@
           UEN
         </label>
       </div>
-
     </div>
 
-    <div class="field  no-print">
+    <div class="field no-print">
       <label for="phone">
-        <h4>{{ mode === 'phone' ? 'Phone' : 'UEN' }}</h4>
+        <h4>{{ mode === "phone" ? "Phone" : "UEN" }}</h4>
       </label>
-      <input type="tel" v-if="mode === 'phone'" id="phone" :value="target"
-        @input="event => target = (event.target as any).value" />
-      <input type="text" v-else id="phone" :value="target" @input="event => target = (event.target as any).value" />
+      <input
+        type="tel"
+        v-if="mode === 'phone'"
+        id="phone"
+        :value="target"
+        @input="event => target = (event.target as any).value"
+      />
+      <input
+        type="text"
+        v-else
+        id="phone"
+        :value="target"
+        @input="event => target = (event.target as any).value"
+      />
     </div>
 
     <div class="field no-print" v-if="mode === 'uen'">
       <label for="reference">
         <h4>Transaction Reference</h4>
       </label>
-      <input type="text" id="reference" :value="reference" @input="event => reference = (event.target as any).value" />
+      <input
+        type="text"
+        id="reference"
+        :value="reference"
+        @input="event => reference = (event.target as any).value"
+      />
     </div>
     <img src="./assets/paynow-logo-2-01.png" ref="logoRef" class="logo" />
 
     <img :src="qrcodeDataURL" class="generated-QR" />
 
-    <canvas ref="canvasRef" width="600" height="600" style="display: none"></canvas>
-    <div class="caption">{{ qrcodeDataTarget.replace(/^\+65/, '') }}</div>
+    <canvas ref="canvasRef" :width="imageWidth" :height="imageWidth" style="display: none"></canvas>
+    <div class="caption">{{ qrcodeDataTarget.replace(/^\+65/, "") }}</div>
     <div style="text-align: center">
       <br />
       <button class="no-print" @click="print">Print</button>
@@ -61,17 +74,18 @@
     <ol>
       <li>
         <a
-          href="https://www.emvco.com/specifications/emv-qr-code-specification-for-payment-systems-emv-qrcps-merchant-presented-mode/">EMV®
-          QR Code Specification for Payment Systems (EMV® QRCPS) Merchant-Presented Mode</a>
+          href="https://www.emvco.com/specifications/emv-qr-code-specification-for-payment-systems-emv-qrcps-merchant-presented-mode/"
+          >EMV® QR Code Specification for Payment Systems (EMV® QRCPS) Merchant-Presented
+          Mode</a
+        >
       </li>
       <li>
         <a href="https://www.fullstacksys.com/paynow-qr-code-generator">
           Fullstacksys.com Paynow QR Code Generator
-        </a> - this website generates QR codes with suggested amounts, but not static codes.
+        </a>
+        - this website generates QR codes with suggested amounts, but not static codes.
       </li>
-      <li>
-        Singapore QR Codes for Payments ("SGQR") Specifications v1.7
-      </li>
+      <li>Singapore QR Codes for Payments ("SGQR") Specifications v1.7</li>
     </ol>
   </footer>
 </template>
@@ -86,8 +100,7 @@
 @media screen {
   .generated-QR {
     max-width: 500px;
-    max-height:
-      500px;
+    max-height: 500px;
   }
 
   header,
@@ -95,8 +108,7 @@
   footer {
     max-width: 520px;
     display: block;
-    margin:
-      auto;
+    margin: auto;
   }
 }
 
@@ -109,7 +121,6 @@ input[type="radio"] {
   margin: 0.8em 0;
 }
 
-
 input[type="text"],
 input[type="tel"] {
   padding: 0.3em;
@@ -119,9 +130,7 @@ input[type="tel"] {
 }
 
 .logo {
-  display: hidden;
-  height: 1px;
-  width: 1px;
+  display: none;
 }
 
 .generated-QR {
@@ -145,139 +154,167 @@ button {
 </style>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-import { sortBy, throttle } from 'lodash'
-import * as qrcode from 'qrcode'
-import crc16ccitt from 'crc/calculators/crc16ccitt'
+import { ref, watch } from "vue";
+import { sortBy, throttle } from "lodash";
+import * as qrcode from "qrcode";
+import crc16ccitt from "crc/calculators/crc16ccitt";
 
-const mode = ref('phone')
-const target = ref('')
-const reference = ref('')
-const canvasRef = ref(null as null | HTMLCanvasElement)
-const logoRef = ref(null as null | HTMLImageElement)
+const mode = ref("phone");
+const target = ref("");
+const reference = ref("");
+const canvasRef = ref(null as null | HTMLCanvasElement);
+const logoRef = ref(null as null | HTMLImageElement);
 
-type QRDataComponentValue = null | string | QRData
+type QRDataComponentValue = null | string | QRData;
 
 class QRData {
-  components: Record<string, QRDataComponentValue>
+  components: Record<string, QRDataComponentValue>;
 
   constructor(components: Record<string, QRDataComponentValue>) {
-    this.components = components
+    this.components = components;
   }
 
   toString(): string {
     return sortBy(Object.entries(this.components), ([k, v]: [string, any]) => k)
       .map(([key, comp]: [key: string, comp: QRDataComponentValue]) => {
         if (!key.match(/^[0-9]{2}$/)) {
-          throw new Error("Key should be a 2-digit numeric key code")
+          throw new Error("Key should be a 2-digit numeric key code");
         }
 
         if (comp === null) {
-          return ''
+          return "";
         }
 
-        const encodedValue = (typeof comp === 'string') ? comp : comp.toString()
+        const encodedValue = typeof comp === "string" ? comp : comp.toString();
 
-        const length = encodedValue.length
+        const length = encodedValue.length;
 
         if (length > 99) {
-          throw new Error("Encoded length should not exceed 99!")
+          throw new Error("Encoded length should not exceed 99!");
         }
 
-        return [
-          key,
-          length.toString().padStart(2, '0'),
-          encodedValue,
-        ].join('')
+        return [key, length.toString().padStart(2, "0"), encodedValue].join("");
       })
-      .join('')
+      .join("");
   }
 
   toStringWithCRC(): string {
-    const result = this.toString() + '6304'
-    const rcode = crc16ccitt(new TextEncoder().encode(result)).toString(16).padStart(4, '0').toUpperCase();
+    const result = this.toString() + "6304";
+    const rcode = crc16ccitt(new TextEncoder().encode(result))
+      .toString(16)
+      .padStart(4, "0")
+      .toUpperCase();
 
-    return result + rcode
+    return result + rcode;
   }
 }
 
-const print = () => window.print()
+const print = () => window.print();
 
 const getLocalStorage = (key: string) => {
   try {
-    return window.localStorage[key]
+    return window.localStorage[key];
   } catch {
-    return ''
+    return "";
   }
-}
+};
 
 const updateLocalStorage = (key: string, value: string) => {
   try {
-    window.localStorage[key] = value
+    window.localStorage[key] = value;
   } catch {
-    return ''
+    return "";
   }
-}
+};
 
-const qrcodeDataURL = ref(getLocalStorage('phone'))
-const qrcodeDataTarget = ref('')
+const qrcodeDataURL = ref(getLocalStorage("phone"));
+const qrcodeDataTarget = ref("");
 
 const standardizePhone = (mode: string, s: string) => {
-  if (mode !== 'phone') {
+  if (mode !== "phone") {
     return s;
   } else {
     if (s.match(/^[0-9]{8}$/)) {
-      return '+65' + s
+      return "+65" + s;
     } else {
-      return s
+      return s;
     }
   }
-}
+};
 
-watch([mode, target, reference, canvasRef], throttle(async () => {
-  const finalTarget = standardizePhone(mode.value, target.value)
-  const data = new QRData({
-    // EMV-Co specs: https://www.emvco.com/specifications/emv-qr-code-specification-for-payment-systems-emv-qrcps-merchant-presented-mode/
-    '00': '01', // Payload format indicator: Version we're using
-    '01': '11', // Point of initiation method: 11 - static, 12 - dynamic
-    '26': new QRData({
-      '00': 'SG.PAYNOW',
-      '01': mode.value === 'phone' ? '0' : '2',
-      '02': finalTarget,
-      '03': '1', // Amount is not editabe
-      '04': mode.value === 'uen' ? reference.value || null : null,
-    }),
-    '52': '0000', // Merchant category code
-    '53': '702', // ISO 4217 code for SGD
-    '58': 'SG',
-    '59': 'NA', // Merchant name -- doesn't matter because PayNow has its own lookup
-    '60': 'Singapore', // City
-  }).toStringWithCRC()
+const imageWidth = 2000;
 
-  updateLocalStorage('phone', target.value)
+watch(
+  [mode, target, reference, canvasRef, imageWidth],
+  throttle(
+    async () => {
+      const finalTarget = standardizePhone(mode.value, target.value);
+      const data = new QRData({
+        // EMV-Co specs: https://www.emvco.com/specifications/emv-qr-code-specification-for-payment-systems-emv-qrcps-merchant-presented-mode/
+        "00": "01", // Payload format indicator: Version we're using
+        "01": "11", // Point of initiation method: 11 - static, 12 - dynamic
+        "26": new QRData({
+          "00": "SG.PAYNOW",
+          "01": mode.value === "phone" ? "0" : "2",
+          "02": finalTarget,
+          "03": "1", // Amount is not editabe
+          "04": mode.value === "uen" ? reference.value || null : null,
+        }),
+        "52": "0000", // Merchant category code
+        "53": "702", // ISO 4217 code for SGD
+        "58": "SG",
+        "59": "NA", // Merchant name -- doesn't matter because PayNow has its own lookup
+        "60": "Singapore", // City
+      }).toStringWithCRC();
 
-  console.log(data)
+      updateLocalStorage("phone", target.value);
 
-  // const dataURL = await qrcode.toDataURL(data, { errorCorrectionLevel: 'H' })
+      console.log(data);
 
-  // qrcodeDataURL.value = dataURL;
-  if (canvasRef.value) {
-    await qrcode.toCanvas(canvasRef.value, data, { errorCorrectionLevel: 'M', width: 600, color: { dark: '#941c80' } })
-    const context = canvasRef.value?.getContext('2d')
+      // const dataURL = await qrcode.toDataURL(data, { errorCorrectionLevel: 'H' })
 
-    const logoElem = logoRef.value
-    if (logoElem) {
-      const width = 1323, height = 894;
-      const targetWidth = 150, targetHeight = height / width * targetWidth
-      context!.fillStyle = 'white'
-      context!.fillRect(300 - targetWidth / 2, 300 - targetHeight / 2, targetWidth, targetHeight)
-      context!.drawImage(logoElem, 300 - targetWidth / 2, 300 - targetHeight / 2, targetWidth, targetHeight)
+      // qrcodeDataURL.value = dataURL;
+      if (canvasRef.value) {
+        await qrcode.toCanvas(canvasRef.value, data, {
+          errorCorrectionLevel: "M",
+          width: imageWidth,
+          color: { dark: "#941c80" },
+        });
+        const context = canvasRef.value?.getContext("2d");
 
-      qrcodeDataURL.value = canvasRef.value.toDataURL()
-      qrcodeDataTarget.value = finalTarget
-    }
-  }
-}, 500, { leading: true, trailing: true }), { immediate: true })
+        const logoElem = logoRef.value;
+      debugger;
+      if (logoElem) {
+          const width = 1323;
+          const height = 894;
+          const targetWidth = imageWidth / 4;
+          const targetHeight = (height / width) * targetWidth;
+          const midPoint = imageWidth / 2;
+          context!.imageSmoothingEnabled=true
+          // context!.imageSmoothingQuality='high'
+          context!.fillStyle = "white";
+          context!.fillRect(
+            midPoint - targetWidth / 2,
+            midPoint - targetHeight / 2,
+            targetWidth,
+            targetHeight
+          );
+          context!.drawImage(
+            logoElem,
+            midPoint - targetWidth / 2,
+            midPoint - targetHeight / 2,
+            targetWidth,
+            targetHeight
+          );
+        }
 
-
+        qrcodeDataURL.value = canvasRef.value.toDataURL();
+        qrcodeDataTarget.value = finalTarget;
+      }
+    },
+    500,
+    { leading: true, trailing: true }
+  ),
+  { immediate: true }
+);
 </script>
